0bf24a74ed59190d960da1bc812dc17b4c0f50a0
astrimbu <astrimbu@gmail.com>
2015-08-26 17:13:52 -0500

    Renamed journal to site

--- site.py
+++ site.py
@@ -5,7 +5,6 @@ from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 
 # configuration
-DATABASE = 'journal.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
