98347dbad47e6f2ad7ea62fee9f80b114820ed88
astrimbu <astrimbu@gmail.com>
2015-08-26 16:56:25 -0500

    Added bare-bones Flask app

---
+++ journal.py
@@ -0,0 +1,43 @@
+# all the imports
+import sqlite3
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+
+# configuration
+DATABASE = 'journal.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'pass'
+
+# create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_entries():
+    return render_template('show_entries.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run()
